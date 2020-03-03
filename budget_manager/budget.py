8dc6b5ea266234f59c21b4024444116ca6d0ea3c
Gian Millare <gianmillare@Gians-MacBook-Pro.local>
2020-03-02 17:00:09 -0800

    created the first function which declares a budget type and amount, and reduces the budget from the total avaialble

--- budget_manager/budget.py
+++ budget_manager/budget.py
@@ -1,3 +1,16 @@
+# Input a starting amount, the total amount you have available
 available = 2500.00
+
+# Create two empty dictionaries for budgets used and how much is expensed
 budgets = {}
-expenditures = {}
+expenditure = {}
+
+# Create a function that will name a budget type and reduce the budget from the total available
+def add_budget(name, amount):
+    global available
+
+    budgets[name] = amount
+    available -= amount
+    expenditure[name] = 0
+    return available
+    
