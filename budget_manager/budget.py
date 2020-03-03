db9f7738adfcce7cd613f0c578484cb934d0464b
Gian Millare <gianmillare@Gians-MacBook-Pro.local>
2020-03-02 18:08:14 -0800

    created the BudgetManager class, created an initializer and made the three main variables into instances using self

--- budget_manager/budget.py
+++ budget_manager/budget.py
@@ -1,64 +1,65 @@
-# Input a starting amount, the total amount you have available
-available = 2500.00
+class BudgetManager:
+    def __init__(self, amount):
+        # Input a starting amount, the total amount you have available
+        self.available = amount
 
-# Create two empty dictionaries for budgets used and how much is expensed
-budgets = {}
-expenditure = {}
+        # Create two empty dictionaries for budgets used and how much is expensed
+        self.budgets = {}
+        self.expenditure = {}
 
-# Create a function that will name a budget type and reduce the budget from the total available
-def add_budget(name, amount):
-    global available
+        # Create a function that will name a budget type and reduce the budget from the total available
+        def add_budget(self, name, amount):
 
-    # Make a function that will return an error if the same budget is used twice
-    if name in budgets:
-        raise ValueError("Budget Exists! Please choose another!")
+            # Make a function that will return an error if the same budget is used twice
+            if name in self.budgets:
+                raise ValueError("Budget Exists! Please choose another!")
 
-    # Make another function that will return an error if the amount of the budget exceeds the available.
-    if amount > available:
-        raise ValueError("Insufficient Funds")
+            # Make another function that will return an error if the amount of the budget exceeds the available.
+            if amount > self.available:
+                raise ValueError("Insufficient Funds")
 
-    budgets[name] = amount
-    available -= amount
-    expenditure[name] = 0
-    return available
-    
-# Create a spending function that will track how much you've spent
-def spend(name, amount):
-    if name not in expenditure:
-        raise ValueError("No Such Budget")
-    expenditure[name] += amount
+            self.budgets[name] = amount
+            self.available -= amount
+            self.expenditure[name] = 0
+            return self.available
+            
+        # Create a spending function that will track how much you've spent
+        def spend(self, name, amount):
+            if name not in self.expenditure:
+                raise ValueError("No Such Budget")
+            self.expenditure[name] += amount
 
-    # Track the amount left in the budget
-    budgeted = budgets[name]
-    spent = expenditure[name]
-    return budgeted - spent
+            # Track the amount left in the budget
+            budgeted = self.budgets[name]
+            spent = self.expenditure[name]
+            return budgeted - spent
 
-# Create a function that will print out a summary of your budget, expenses, and avaialble balance
-def print_summary():
-    # Create a format table to make the summary easier to read
-    print("Budget              Budgeted   Spent Remaining")
-    print("---------------------------------------------------------")
+        # Create a function that will print out a summary of your budget, expenses, and avaialble balance
+        def print_summary(self):
+            # Create a format table to make the summary easier to read
+            print("Budget              Budgeted   Spent Remaining")
+            print("---------------------------------------------------------")
 
-    # Create a "totals" variable for a footer section in the summary
-    total_budgeted = 0
-    total_spent = 0
-    total_remaining = 0
-    
-    for name in budgets:
-        budgeted = budgets[name]
-        spent = expenditure[name]
-        remaining = budgeted - spent
+            # Create a "totals" variable for a footer section in the summary
+            total_budgeted = 0
+            total_spent = 0
+            total_remaining = 0
+            
+            for name in self.budgets:
+                budgeted = self.budgets[name]
+                spent = self.expenditure[name]
+                remaining = budgeted - spent
 
-        # Start formatting the summary to be easier to read
-        print(f'{name:15s} {budgeted:10.2f} {spent:10.2f} {remaining:10.2f}')
+                # Start formatting the summary to be easier to read
+                print(f'{name:15s} {budgeted:10.2f} {spent:10.2f} {remaining:10.2f}')
 
-        # Add the amounts to the totals before this function
-        total_budgeted += budgeted
-        total_spent += spent
-        total_remaining += remaining
+                # Add the amounts to the totals before this function
+                total_budgeted += budgeted
+                total_spent += spent
+                total_remaining += remaining
 
-    # Create the Footer
-    print("---------------------------------------------------------")
-    print(f'{"Total":15s} {total_budgeted:10.2f} {total_spent:10.2f} {total_budgeted - total_spent:10.2f}')
-    
+            # Create the Footer
+            print("---------------------------------------------------------")
+            print(f'{"Total":15s} {total_budgeted:10.2f} {total_spent:10.2f} {total_budgeted - total_spent:10.2f}')
+            
 
